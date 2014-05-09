ea49e9a14a7cfd92a4fd8dc616bb7fadc6ebaec9
grayayer <grayayer@gmail.com>
2014-05-08 19:31:07 -0700

    additions of sass options
    this is so that I can use this with more flexibility

--- js/script.js
+++ js/script.js
@@ -11,3 +11,41 @@ jQuery(window).load(function() {
         jQuery(document).foundation();
     }
 });
+
+/** Smooth Scrolling Functionality **/
+var jump=function(e)
+{
+    //alert('here');
+   if (e){
+       //e.preventDefault();
+       var target = jQuery(this).attr("href").replace('/', '');
+   }else{
+       var target = location.hash;
+   }
+
+   jQuery('html,body').animate(
+   {
+       scrollTop: (jQuery(target).offset().top) - 100
+   },500,function()
+   {
+       //location.hash = target;
+   });
+
+}
+
+jQuery('html, body').hide();
+
+jQuery(document).ready(function($)
+{
+    $(document).on('click', 'a[href*=#]', jump);
+
+    if (location.hash){
+        setTimeout(function(){
+            $('html, body').scrollTop(0).show();
+            jump();
+        }, 0);
+    }else{
+        $('html, body').show();
+    }
+});
+/** END SMOOTH SCROLLING FUNCTIONALITY **/
