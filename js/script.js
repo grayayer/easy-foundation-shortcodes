1284b16d99f958c4fef5aa7404d43bdc63eb11a6
grayayer <grayayer@gmail.com>
2014-05-09 13:25:10 -0700

    added gravity form placeholder clearing form

--- js/script.js
+++ js/script.js
@@ -49,3 +49,23 @@ jQuery(document).ready(function($)
     }
 });
 /** END SMOOTH SCROLLING FUNCTIONALITY **/
+
+
+/**** allows for placeholder on contact form ***/
+jQuery(document).ready(function() {
+
+  jQuery.fn.cleardefault = function() {
+  return this.focus(function() {
+    if( this.value == this.defaultValue ) {
+      this.value = "";
+    }
+  }).blur(function() {
+    if( !this.value.length ) {
+      this.value = this.defaultValue;
+    }
+  });
+};
+jQuery(".clearit input, .clearit textarea").cleardefault();
+
+});
+
